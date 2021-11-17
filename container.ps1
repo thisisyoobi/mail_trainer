5bbdf3c1188954d69bccdfb8ac22658ac049e7f3
이종학 <52825485+hak-2@users.noreply.github.com>
2021-11-17 18:51:36 +0900

    Update container.ps1

--- container.ps1
+++ container.ps1
@@ -1,3 +1,4 @@
+powershell -Command "docker login"
 powershell -Command "docker pull dlwhdgkr98/project_test:2"
 powershell -Command "docker run -it --name mail_trainer -p 3306:3306 -p 3307:21 -p 8080:8080 dlwhdgkr98/project_test:2 /bin/bash"
 
